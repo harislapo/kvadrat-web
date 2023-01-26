39d76cd17ebafb433209c8ce5c1eab56bf54792d
harislapo <haris.lapo@gmail.com>
2023-01-26 15:39:52 +0100

    Styled Founder component.

--- src/container/Founder/Founder.jsx
+++ src/container/Founder/Founder.jsx
@@ -1,8 +1,53 @@
 import React from 'react';
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
 import './Founder.css';
 
 const Founder = () => {
-  return <div id="founder">Founder</div>;
+  return (
+    <div className="app__wrapper section__padding" id="founder">
+      <div className="app__wrapper_img app__wrapper_img-reverse">
+        <img
+          src={images.founder}
+          alt="Founder's image"
+          className="white-border white-border-none"
+        />
+      </div>
+      <div className="app__wrapper_info">
+        <SubHeading title="Kvadrat's founder" />
+        <h1 className="headtext__cormorant">Adnan Omerović</h1>
+        <div className="app__founder-content">
+          <p className="p__opensans">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
+            doloribus pariatur voluptatem sed officiis! Quod repudiandae
+            corrupti laudantium ab soluta dolorem necessitatibus repellendus,
+            facere deserunt neque ut in eum voluptate. Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Id doloribus pariatur voluptatem
+            sed officiis! Quod repudiandae corrupti laudantium ab soluta dolorem
+            necessitatibus repellendus, facere deserunt neque ut in eum
+            voluptate. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Id doloribus pariatur voluptatem sed officiis! Quod repudiandae
+            corrupti laudantium ab soluta dolorem necessitatibus repellendus,
+            facere deserunt neque ut in eum voluptate. Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Id doloribus pariatur voluptatem
+            sed officiis! Quod repudiandae corrupti laudantium ab soluta dolorem
+            necessitatibus repellendus, facere deserunt neque ut in eum
+            voluptate. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Id doloribus pariatur voluptatem sed officiis! Quod repudiandae
+            corrupti laudantium ab soluta dolorem necessitatibus repellendus,
+            facere deserunt neque ut in eum voluptate. Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Id doloribus pariatur voluptatem
+            sed officiis! Quod repudiandae corrupti laudantium ab soluta dolorem
+            necessitatibus repellendus, facere deserunt neque ut in eum
+            voluptate. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Id doloribus pariatur voluptatem sed officiis! Quod repudiandae
+            corrupti laudantium ab soluta dolorem necessitatibus repellendus,
+            facere deserunt neque ut in eum voluptate.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Founder;
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -10,11 +10,16 @@ const Header = () => {
         <SubHeading title="Where architecture comes to life." />
         <h1 className="app__header-h1">Kvadrat Atelje</h1>
         <p className="p__opensans" style={{ margin: '2rem 0' }}>
-          Architectural studio &#8212; based in Sarajevo, Bosnia and Herzegovina.
+          Architectural studio &#8212; based in Sarajevo, Bosnia and
+          Herzegovina.
         </p>
       </div>
-      <div className="app__wrapper_img">
-        <img src={images.header_img} alt="header image" />
+      <div className="app__wrapper_img ">
+        <img
+          src={images.header_img}
+          alt="header image"
+          className="white-border"
+        />
       </div>
     </div>
   );
